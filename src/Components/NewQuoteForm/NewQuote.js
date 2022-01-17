7898a3acec14cfdbd486c53c4f39d8e172e8f783
Vali <coder@kali.kali>
2022-01-18 03:19:20 +0500

    new quote pages

--- src/Components/NewQuoteForm/NewQuote.js
+++ src/Components/NewQuoteForm/NewQuote.js
@@ -1,10 +1,20 @@
-import { motion } from "framer-motion"
 import { useState } from "react"
 import {BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom'
+import ContactInformation from "./ContactInformation"
+import QuoteSubmitted from "./QuoteSubmitted"
 import VehicleInformation from "./VehicleInformation"
 const NewQuote = () => {
     const [shipFrom, setShipFrom] = useState('')
     const [shipTo, setShipTo] = useState('')
+    const [year, setYear] = useState('')
+    const [make, setMake] = useState('')
+    const [model, setModel] = useState('')
+    const [type, setType] = useState('')
+    const [isRunning, setIsRunning] = useState('')
+    const [name, setName] = useState('')
+    const [number, setNumber] = useState('')
+    const [email, setEmail] = useState('')
+    const [date, setDate] = useState('')
     const handleSubmit = (e)=>{
         console.log(shipFrom, shipTo)
         e.preventDefault()
@@ -12,7 +22,7 @@ const NewQuote = () => {
     return ( 
         <form onSubmit={(e)=>handleSubmit(e)} className='quote-form'>
             <h1>GET A FREE QUOTE NOW</h1>
-            <p initial={{scale:0}} animate={{scale:1, originX:0}} transition={{ type: "spring"}}>(929) 419-5297</p>
+            <p>(929) 419-5297</p>
             <div  className="or">
                 <span className='left-hand-of-or'></span>
                 <span>OR</span>
@@ -28,8 +38,14 @@ const NewQuote = () => {
                             <Link to='/vehicle_information'><button>NEXT</button></Link>
                     </Route>
                     <Route path='/vehicle_information'>
-                        <VehicleInformation />
-                    </Route> 
+                        <VehicleInformation year = {year} setYear = {setYear} make={make} setMake={setMake} model={model} setModel={setModel}  setType ={setType}  setIsRunning = {setIsRunning}/>
+                    </Route>
+                    <Route path='/contact_information'>
+                        <ContactInformation name={name} setName = {setName} number={number} setNumber = {setNumber} email ={email} setEmail = {setEmail} date = {date} setDate={setDate} />
+                    </Route>
+                    <Route path="/quote_submitted">
+                        <QuoteSubmitted />
+                    </Route>
                 </Switch>
             </Router>
         </form>
--- src/Components/NewQuoteForm/VehicleInformation.js
+++ src/Components/NewQuoteForm/VehicleInformation.js
@@ -1,40 +1,48 @@
 import { useState } from 'react';
-import { useHistory } from 'react-router-dom'
-const VehicleInformation = () => {
+import { useHistory, Link } from 'react-router-dom'
+const VehicleInformation = ({year, setYear, make, setMake, model, setModel, setType, setIsRunning}) => {
     const [vehicleCount, setVehicleCount]=useState([1])
+    const handleVehicleCount = ()=>{
+        setVehicleCount([...vehicleCount, 1])
+    }
     const history = useHistory()
     return ( 
     <>
         <p>Vehicle Information:</p>
-        { vehicleCount.map(i=>{
+        { vehicleCount.map((i, index)=>{
             return(
-                <>
+                <div key={index}>
+                    {index>0&&
+                    <div className='indicator-and-close'>
+                        <p>Vehicle:{index+1}</p>
+                        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12 19 6.41z"/></svg>
+                    </div>}
                     <label htmlFor="year">Year</label>
-                    <input type="text"  name='year' placeholder='Year' required/>
+                    <input value={year} onChange={e=>setYear(e.target.value)} type="text"  name='year' placeholder='Year' required/>
                     <label htmlFor="make">Make</label>
-                    <input type="text"  name='make' placeholder='Make' required/>
+                    <input value={make} onChange={e=>setMake(e.target.value)} type="text"  name='make' placeholder='Make' required/>
                     <label htmlFor="model">Model</label>
-                    <input type="text"  name='model' placeholder='Model' required/>
+                    <input value={model} onChange={e=>setModel(e.target.value)} type="text"  name='model' placeholder='Model' required/>
                     <p>Trailer Type:</p>
                     <label>
-                        <input type="radio" name='type' />Open
+                        <input onChange={e=>setType('Open')} type="radio" name='type' required/>Open
                     </label>
                     <label >
-                        <input type="radio" name='type' />Enclosed
+                        <input onChange={e=>setType('Enclosed')} type="radio" name='type' required/>Enclosed
                     </label>
                     <p>Running?</p>
                     <label>
-                        <input type="radio" name='runs'/>Yes
+                        <input onChange={e=>setIsRunning('Yes')}  type="radio" name='runs' required/>Yes
                     </label>
                     <label>
-                        <input type="radio" name='runs'/>No
+                        <input onChange={e=>setIsRunning('No')}  type="radio" name='runs' required/>No
                     </label>
-                </>
+                </div>
             )
         })}
-        <p>Add Another Vehicle</p>
+        <p onClick={handleVehicleCount}>Add Another Vehicle</p>
         <button onClick={()=>history.push('/')}>Previous</button>
-        <button>Next</button>
+        <Link to='/contact_information'><button>Next</button></Link>
     </> 
     );
 }
