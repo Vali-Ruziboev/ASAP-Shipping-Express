ff590e84e97547b64fa56fbef3f0ba9fa1f0b99d
Vali <coder@kali.kali>
2022-01-30 08:01:01 +0500

    testimonial page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './Components/Header';
+import Testimonial from './Components/Testimonial';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Testimonial />
     </div>
   );
 }
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -40,6 +40,7 @@ const Header = () => {
                             initial={{scale:0}} 
                             animate={{scale:1, transition:{type:"spring",stiffness:250}}} 
                             exit={{scale:0}}>
+                            <motion.svg animate={{rotate:360, transition:{duration:0.4, delay:0.2}}} xmlns="http://www.w3.org/2000/svg" height="24px" strokeWidth="4" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12 19 6.41z"/></motion.svg>
                                 <NavBar/>
                             </motion.div>
                         
