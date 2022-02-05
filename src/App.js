3c52b1457dcaf664c465a88df3f43b9550656873
Vali <coder@kali.kali>
2022-02-05 07:51:10 +0500

    seal and states css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './Components/Header';
 import Testimonial from './Components/Testimonial';
+import WeShipEveryWhere from './Components/WeShipEveryWhere';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Testimonial />
+      <WeShipEveryWhere />
     </div>
   );
 }
