fcb8f25b31187c4a9ff5c7862212b91f06b90d2b
Vali <valiruziboyev212@gmail.com>
2022-02-16 07:25:37 +0500

    faqs functions

--- src/Components/Faqs.js
+++ src/Components/Faqs.js
@@ -1,69 +1,33 @@
 import { useLocation } from "react-router-dom";
+import { TransitionGroup, CSSTransition  } from 'react-transition-group';
 import Header from "./Header";
+import { faqsData } from "./faqsData";
+import { useState } from "react";
 
 const Faqs = () => {
+    const [clicked, setClicked] = useState(null)
+    const handleClick = (i)=>{
+        if(clicked ===i){
+            setClicked(null)
+        }else{
+            setClicked(i)
+        }
+    }
     return ( 
         <>
             <Header />
             <div className="faqs">
                 <h2>Frequently Asked Questions</h2>
                 <div className="faqs-container">
-                    <div className="question">
-                        <h3>How much does it cost to ship a car?</h3>
-                        <p>When determining your car shipping cost, there are a few factors that we take into account in order to provide you with an accurate price quote. The mileage between the origin and destination is the biggest factor that contributes to your total rate. In addition to the distance of your relocation, another factor is the type and condition of the vehicle being shipped. The final factor is the date that your vehicle will be ready for transport.</p>
-                    </div>
-                    <div className="question">
-                        <h3>Open or Enclosed Transport? </h3>
-                        <p>That depends on many factors. Open auto transport is a service where we ship your vehicle on an open carrier. It is a fully insured form of auto transport (that comes with a really affordable price) and provides protection from most damages that may happen to your vehicle during transport. Enclosed auto transport uses a covered, contained transport truck to move your vehicle and usually comes in two varieties, hard or soft-shell. Enclosed trailers do not leave vehicles open to the elements or damage as previously mentioned in the open carrier, but they come at a higher cost than open transportation</p>
-                    </div>
-                    <div className="question">
-                        <h3>Do I have to drop my car off somewhere?</h3>
-                        <p>Most likely not. Most all auto transport companies use what is known as door-to-door service, which is where the driver comes straight to your door and picks up the vehicle, and then delivers it right to your new home (or wherever else it is going to). Certain circumstances may force you to take your car somewhere like a parking lot, but that only happens if the truck cannot get into the neighborhood or is prevented from doing so (some cities have laws restricting large vehicles from driving in neighborhoods). If this is the case, the driver will ask you to take your car to a large area where they can then load the truck on their auto transport truck</p>
-                    </div>
-                    <div className="question">
-                        <h3>Does someone have to be present for pick up and delivery?</h3>
-                        <p>Yes, there must be someone present to release and accept the vehicle, provide keys to the driver and sign the initial and final inspection reports. This person can be anyone over the age of 18 that you trust with releasing or accepting your vehicle.</p>
-                    </div>
-                    <div className="question">
-                        <h3>Will my vehicle stay on the same truck for the entire transport?</h3>
-                        <p>Yes. Once your vehicle is scheduled for pickup, you will receive the name of the carrier company and the driver that is picking up your vehicle. Some areas may require a local tow company to pick your vehicle up and then deliver it to a facility where the long-distance driver can then load it. This is usually the case for cities where the long-distance driver cannot physically reach the pickup location.</p>
-                    </div>
-                    <div className="question">
-                        <h3>Do I need the title of the vehicle in order for it to be shipped?</h3>
-                        <p>No. The reason why is many people ship vehicles that once belonged to relatives or they are acquiring auto transport for friends or family. Most auto transport carriers only require you to have the keys to the vehicle so they can drive it onto the truck, but other than that nothing else is required.</p>
-                    </div>
-                    <div className="question">
-                        <h3>My vehicle doesn't run, can I still ship it?</h3>
-                        <p>Of course! Vehicles that do not run, however, are subject to a non-operational fee, sometimes known as an inop-fee. Check with your auto shipping company to find out how much they charge for non-operational or inop vehicles. The reason why they charge extra for non-operational vehicles is that the carrier will typically drive vehicles onto the truck to load them. However, if a vehicle does not run, special equipment is required to winch the vehicle into position and to secure it, which takes time and effort, and not all carriers have a winch. Check with a representative as to how much extra you need to pay if your vehicle is not running.</p>
-                    </div>
-                    <div className="question">
-                        <h3>Can I ship personal belongings in my vehicle?</h3>
-                        <p>No. Auto transport carriers are licensed only for vehicles, not household items. This means they cannot legally transport anything but vehicles. Not only are they not licensed for household goods, but all auto shipping trucks are required to make regular stops at weigh stations to assess if their vehicle is overweight. If vehicles have extraneous items in them, they can push them over the maximum allowed weight. If this happens, drivers are forced to unload any extra weight before they can continue on their route. Many trucks, however, allow what is known as “junk in the trunk” - about 50-100 pounds of linens, clothes, or other lightweight, non-perishable items in the trunk. The cab of the vehicle (the front and back seats) must be clear of any goods.</p>
-                    </div>
-                    <div className="question">
-                        <h3>How many days does it take to my vehicle be picked up?</h3>
-                        <p>Pickup depends on several factors, but mainly location. The auto shipping industry is route-based, which means that some places will have lots of trucks along routes servicing them, and others will have very few trucks. Most auto transport companies will give you what is known as a pickup window - a range of dates that the vehicle should be picked up within. so be sure to speak to a representative about your pickup window if necessary.</p>
-                    </div>
-                    <div className="question">
-                        <h3>Can I track my vehicle while it is in transport?</h3>
-                        <p>Yes. You should know that with the advent of satellite tracking and GPS systems, many auto transport companies offer some sort of tracking. However, tracking via satellite and GPS is expensive, and many auto transport companies are simply owner-operators who do not own any GPS systems; they are reserved mainly for fleets of auto shipping carriers. Contact your car shipping representative about whether or not they offer to track while your vehicle is in transit. If GPS tracking is unavailable, other forms of tracking, such as calling and asking the driver where they are, are used.</p>
-                    </div>
-                    <div className="question">
-                        <h3>Is my vehicle insured during transport?</h3>
-                        <p>All auto transport carriers carry a federally mandated amount of both liability and cargo insurance. As long as your vehicle is on a car transport truck it is insured through that carrier's insurance, and your personal insurance becomes secondary.</p>
-                    </div>
-                    <div className="question">
-                        <h3>When will my vehicle be delivered?</h3>
-                        <p>The transit time for your shipment is based mostly on the mileage between the pickup and delivery location. Generally, every five hundred miles will add one day to the transit time - so for a 1,000 mile transport, the transit time would be 2-3 days. Check out our estimated transit times for a detailed view of approximate delivery times.</p>
-                    </div>
-                    <div className="question">
-                        <h3>My vehicle was damaged during transport, what do I do?</h3>
-                        <p>Any damages that are incurred during transit will be repaired at the expense of the trucking company's insurance. The driver, upon pickup, will walk around the vehicle with you and inspect it for any prior damages and note them on the report. This is to ensure that any new damages that occur can be documented and separated from any original damage. After the vehicle is delivered, you and the driver will again inspect the vehicle, and any new damages must be documented accordingly. This involves pictures of the damages as well as a price estimate from a repair shop - from there, the truck's insurance will take care of the rest.</p>
-                    </div>
-                    <div className="question">
-                        <h3>How do I pay for my auto shipping?</h3>
-                        <p>We accept all major credit cards, electronic transfer. If a balance is due upon delivery, it can be paid directly to the driver and can be paid in cash, cashier's check, company check, or postal money order.</p>
-                    </div>
+                {faqsData.map((d, i)=>{
+                    return (<div key={i} className="question">
+                        <div onClick={()=>handleClick(i)} className="head">
+                            <h3>{d.question}</h3>
+                            {clicked===i?<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M19 13H5v-2h14v2z"/></svg>:<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z"/></svg>}
+                        </div>
+                            {clicked===i&&<p>{d.answer}</p>}
+                    </div>)
+                })}
                 </div>
             </div>
         </>
