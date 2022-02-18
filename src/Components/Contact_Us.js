72b62a3c9c9f27ce075dfa3307697c1fe13fdb10
Vali <valiruziboyev212@gmail.com>
2022-02-18 07:15:22 +0500

    contact_us

--- src/Components/Contact_Us.js
+++ src/Components/Contact_Us.js
@@ -4,7 +4,18 @@ const Contact_Us = () => {
     return ( 
         <div>
             <Header />
-            Contact Us
+            <div className="contact_us_wrapper">
+                <form className="contact_us">
+                    <h3>Contact Us</h3>
+                    <label htmlFor="name">Your name</label>
+                    <input type="text" name="name" placeholder="Your name"/>
+                    <label htmlFor="email">Your email</label>
+                    <input type="email"  name="email" placeholder="Your email"/>
+                    <label htmlFor="content">Your message</label>
+                    <textarea name="content"cols="30" rows="10" placeholder="Your message"></textarea>
+                    <button type="submit">Send</button>
+                </form>
+            </div>
         </div>
     );
 }
