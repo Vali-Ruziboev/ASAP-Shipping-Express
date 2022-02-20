f4115826a0cd694ccdc1a48081e45b4438bea234
Vali <valiruziboyev212@gmail.com>
2022-02-20 07:17:34 +0500

    email not sending fix

--- src/Components/Contact_Us.js
+++ src/Components/Contact_Us.js
@@ -1,28 +1,33 @@
 import { useState, useRef } from "react";
 import Header from "./Header";
-import emailjs from '@emailjs/browser';
-
+import emailjs, {init} from '@emailjs/browser';
+init("user_1Ig2WajVAhHklQ3Nutlw2")
 const Contact_Us = () => {
     const [name, setName] = useState('')
     const [email, setEmail] = useState('')
     const [content, setContent] = useState('')
-    const form = useRef()
-
+    const form = {
+        email:email,
+        name: name,
+        content:content
+    }
     const handleSubmit = (e)=>{
         e.preventDefault()
-        emailjs.sendForm('send_email_to_myself', 'template_s8qxzx7', form.current, 'user_1Ig2WajVAhHklQ3Nutlw2')
-            .then((result) => {
-                console.log(result.text);
-            }, (error) => {
-                console.log(error.text);
+        emailjs.send('send_email_to_myself', 'template_s8qxzx7', form)
+            .then(function(response) {
+                setName('')
+                setEmail('')
+                setContent('')
+            }, function(error) {
+                console.log('FAILED...', error);
             });
-        console.log(name, email, content);
+        
     }
     return ( 
         <div>
             <Header />
             <div className="contact_us_wrapper">
-                <form ref={form} className="contact_us" onSubmit={(e)=>handleSubmit(e)}>
+                <form className="contact_us" onSubmit={(e)=>handleSubmit(e)}>
                     <h3>Contact Us</h3>
                     <label>Your name *<input value={name} onChange={(e)=>setName(e.target.value)} required type="text" name="name" placeholder="Your name"/></label>
                     <label htmlFor="email">Your email *<input value={email} onChange={(e)=>setEmail(e.target.value)} required type="email"  name="email" placeholder="Your email"/></label>            
